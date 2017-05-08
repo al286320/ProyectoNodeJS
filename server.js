e7f4e16c1818de8b2c46baeccd4a05b213eb226a
al286320 <al286320@uji.es>
2017-05-08 17:00:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -91,14 +91,12 @@ app.get('/dataset/stream/graph', function(req, res){
 //    })
 //});
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '172.30.156.115'
 
 
 
 //Levanta el servidor cuando la BD este lista
 db.warmupEmmitter.once("warmup",() => {
-  app.listen(server_port, server_ip_address, function(){
+  app.listen(8080, function(){
     console.log("Listening on " + server_ip_address + ", server_port " + server_port)
   });
    //console.log("Web server running on port 8000");
